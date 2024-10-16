5c18bee5b95f3c3d55c8498082a1baf51d77f0f2
vivekkum <vivekkum@adobe.com>
2024-10-17 03:45:34 +0530

    set xcodescheme params

--- README.md
+++ README.md
@@ -10,4 +10,5 @@ Build using Xcode
 1. mkdir build-xcode && cd build-xcode
 2. cmake .. -G Xcode
 3. open *.xcodeproj
-4. Run target antlr-cpp-test (the choice of target is on the top)
+4. Open hello.cc and set a breakpoint in Init (Cmd + Shift + O to search hello.cc)
+5. Run target node-addon (the choice of target is on the top)
--- node-addon/hello.js
+++ node-addon/hello.js
@@ -1,11 +1,10 @@
 const process = require('process');
-console.log("Current working directory: ",
-    process.cwd());
+console.log("Current working directory: ", process.cwd());
 
     try {
-        var addon = require("./../build/node-addon/Release/node-addon.node");
+        var addon = require(process.cwd() + "/Release/node-addon.node");
     } catch(e) {
-        var addon = require("./../build/node-addon/Debug/node-addon.node");
+        var addon = require(process.cwd() + "/Debug/node-addon.node");
     }
 
 console.log(addon.hello()); // 'world'
